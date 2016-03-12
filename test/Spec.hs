0203843694a46d709f88bdf32d9b11883919d972
Colin Man <colinman18@gmail.com>
2016-03-12 15:23:44 -0800

    couple edits to add state to testing

--- test/Spec.hs
+++ test/Spec.hs
@@ -16,30 +16,41 @@ import Network.BSD as BSD
 import Control.Concurrent
 import Control.Concurrent.MVar
 
-
 import System.Process
 
 import ClientLib
 import Lib
 import KVProtocol
 
+import Control.Monad.State
 import Control.Monad.Trans (liftIO)
 
 import Data.Maybe
 
+data TestState = TestState {
+   masterHandles :: [ProcessHandle], 
+   clientHandles :: [ProcessHandle],
+   slaveHandles :: [ProcessHandle]
+}
+
 main :: IO ()
 main = do
-  setUpTopology
-  hspec $ do
-    getTests
-	--getTests
-	--putTests
+  (s, a) <- runStateT runTests $ TestState [] [] []
+  return ()
 
-setUpTopology :: IO ()
-setUpTopology = do
-  spawnCommand "stack exec -- kvstore-master --local --size=1" --"stack" ["exec", "--", "kvstore-master", "--local", "--size=1"] []
-  spawnCommand "stack exec -- kvstore-slave --local --size=1 --id=0"
-  spawnCommand "stack exec -- kvstore-client --local --size=1"
+runTests :: StateT TestState IO ()
+runTests = do
+  setUpTopology
+  liftIO $ hspec $ do
+    getTests
+
+setUpTopology :: StateT TestState IO ()
+setUpTopology = get >>= \s -> do
+  m <- liftIO $ spawnCommand "stack exec -- kvstore-master --local --size=1"
+  sl <- liftIO $ spawnCommand "stack exec -- kvstore-slave --local --size=1 --id=0"
+  c <- liftIO $ spawnCommand "stack exec -- kvstore-client --local --size=1"
+
+  put s { masterHandles = [m], clientHandles = [c], slaveHandles = [sl] }
   return ()
 
 
@@ -48,13 +59,12 @@ getTests = describe "test get requests" $ do
   describe "single get request issued" $ do
     it "(key,val) -> val" $ do
       --portId@(PortNumber pid) <- NETWORK.socketPort s
-  	  --hostName <- BSD.getHostName
-      let hostName = "blah"
+      --hostName <- BSD.getHostName
       let pno = PortNumber 1
       let masterPortId = PortNumber 1           
       let clientCfg = [("127.0.0.1", PortNumber 1064)]
       let slaveCfg = [("blee", PortNumber 2)]
-      let overallCfg = Lib.Config hostName pno clientCfg slaveCfg Nothing Nothing
+      let overallCfg = Lib.Config "" pno clientCfg slaveCfg Nothing Nothing
 
       finCfg <- registerWithMaster overallCfg
       let expOutput = C8.pack "val"
